9127527cedb01de6b1c3f80ff7211955784a44d6
Matt Averill <mpaverill@gmail.com>
2021-01-27 17:04:07 -0500

    created database and connected to it with knex

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const cors = require('cors')
+const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'testpassword',
+    database : 'smartbrain'
+  }
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app = express();
 app.use(express.json());
