52b4410b1fd22fac6b50bc83de1e5760d8891247
Matt <mpaverill@gmail.com>
2021-03-22 11:22:34 -0400

    trying to finalize

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const handleApiCall = (req, res) =>{
 	.catch(err => res.status(400).json('unable to work with API'))
 }
 
+//connects to the database
 const db = knex({
   client: 'pg',
   connection: {
@@ -27,13 +28,16 @@ const db = knex({
 });
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
+//root route
 app.get('/', (req,res)=> {
 	res.send(db.users);
 })
 
+//sign in route
 app.post('/signin', (req,res)=>{
 	const {email, password } = req.body;
 	if (!email || !password){
@@ -47,6 +51,7 @@ app.post('/signin', (req,res)=>{
 			return db.select('*').from('users')
 			.where('email', '=', email)
 			.then(user => {
+				console.log(user);
 				res.json(user[0])
 			})
 			.catch(err => res.status(400).json('unable to get user'))
@@ -55,6 +60,7 @@ app.post('/signin', (req,res)=>{
 	.catch(err => res.status(400).json('wrong credentials'))
 })
 
+//register route
 app.post('/register', (req,res)=>{
 	const { email, name, password } = req.body;
 	if (!email || !name || !password){
@@ -88,34 +94,38 @@ app.post('/register', (req,res)=>{
 	.catch(err => res.status(400).json('unable to register'))
 })
 
+//profile route
 app.get('/profile/:id', (req, res)=>{
 	const { id } = req.params;
-	db.select('*').from('users').where(
-		{
-			id: id
-		}).then(user => {
+	db.select('*').from('users').where({id})
+		.then(user => {
 			if (user.length){
 				res.json(user[0]);
 			} else{
 				res.status(400).json('Not Found')
 			}
-		}).catch(err => res.status(400).json('Error Getting User'))
+		})
+		.catch(err => res.status(400).json('Error Getting User'))
 })
 
+//image route
 app.put('/image', (req,res)=>{
-	const {id} = req.body;
+	const { id } = req.body;
 	db('users').where('id', '=', id)
 	.increment('entries', 1)
 	.returning('entries')
 	.then(entries => {
 		res.json(entries[0]);
-	}).catch(err => res.status(400).json('Unable to get entries'))
+	})
+	.catch(err => res.status(400).json('Unable to get entries'))
 })
 
+//image url route
 app.post('/imageurl', (req, res) => {
 	handleApiCall(req, res)
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
-	console.log(`app is running on port ${process.env.PORT}`)
+//port listening
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
 })
